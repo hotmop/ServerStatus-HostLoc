252dea6e7cb0918733553be64374118626c1a2a3
hotmop <hotmop@163.com>
2022-03-17 15:49:47 +0800

    Update status.sh

--- status.sh
+++ status.sh
@@ -644,7 +644,7 @@ Install_caddy(){
 		Set_server "server"
 		Set_server_http_port
 		if [[ ! -e "/usr/local/caddy/caddy" ]]; then
-			wget -N --no-check-certificate https://raw.githubusercontent.com/hotmop/ServerStatus-HostLoc/master/caddy_install.sh
+			wget -N --no-check-certificate https://raw.githubusercontent.com/iiiiiii1/doubi/master/caddy_install.sh
 			chmod +x caddy_install.sh
 			bash caddy_install.sh install
 			rm -rf caddy_install.sh
